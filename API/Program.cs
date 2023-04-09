bceabf11021b35b256a58cda92a2606421613168
Rakesh654 <rk9.kumar21@gmail.com>
2023-04-09 15:21:07 +0530

    Client View Setup

--- API/Program.cs
+++ API/Program.cs
@@ -11,8 +11,9 @@ builder.Services.AddDbContext<DataContext>(x =>
     x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 
 });
+builder.Services.AddCors();
 var app = builder.Build();
-
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.MapControllers();
 
 app.Run();
