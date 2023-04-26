d5329b20f82ced2b1bd9b4e6d9247ef9a53d3e96
Rakesh654 <rk9.kumar21@gmail.com>
2023-04-26 08:33:41 +0530

    Added Guard,Shared Module and bootswatch theme

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { AccountService } from '../_services/account.service';
-import { Observable, of } from 'rxjs';
-import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +11,7 @@ import { User } from '../_models/user';
 export class NavComponent {
   model: any={};
 
-  constructor(public accountService : AccountService){
+  constructor(public accountService : AccountService, private router: Router, private toaster : ToastrService){
 
   }
   ngOnInit():void
@@ -21,16 +21,15 @@ export class NavComponent {
   login()
   {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response)
-      },
-      error: error => console.log(error)
+      next: () => this.router.navigateByUrl('/members'),
+      error: error => this.toaster.error(error.error)
     });
   }
 
   logout()
   {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -11,7 +12,7 @@ export class RegisterComponent {
   @Output() cancelRegister = new EventEmitter();
   model : any ={}
 
-  constructor(private http: HttpClient, private accountService: AccountService)
+  constructor(private http: HttpClient, private accountService: AccountService, private toaster : ToastrService)
   {
 
   }
@@ -25,10 +26,11 @@ export class RegisterComponent {
     this.accountService.register(model).subscribe({
       next : () =>{
         this.cancel();
+        
       },
       error : error =>
       {
-        console.log(error);
+        this.toaster.error(error.error);
       }
     });
   }
